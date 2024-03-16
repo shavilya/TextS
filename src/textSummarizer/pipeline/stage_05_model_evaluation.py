c977cf24855dde08d46b34886dd94cfdb6b677d4
shavilya <shavilyarajput50@gmail.com>
2024-03-16 20:04:59 +0530

    Model Training and Evaluation done

---
+++ src/textSummarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,21 @@
+from transformers import TrainingArguments, Trainer
+from transformers import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from datasets import load_dataset, load_from_disk
+from textSummarizer.entity import ModelTrainerConfig
+import torch
+import os
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.conponents.model_evaluation import ModelEvaluation
+from textSummarizer.logging import logger
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
